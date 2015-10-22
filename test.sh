65732dde633b8339d79c3cd53a9f39fa71095c32
Jessica Frazelle <acidburn@docker.com>
2015-10-22 04:43:46 -0700

    pass
    Signed-off-by: Jessica Frazelle <acidburn@docker.com>

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+(
 # find all executables and run `shellcheck`
 for f in $(find . -type f -executable); do
 	shellcheck $f && echo -e "---\nSucessfully linted $f\n---"
 done
+) || true
