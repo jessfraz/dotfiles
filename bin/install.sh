1355692ec6dd0b6b7f115eda19e8212796fda94a
Jess Frazelle <acidburn@microsoft.com>
2018-06-12 08:06:00 -0400

    use makefile for .vim install
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- bin/install.sh
+++ bin/install.sh
@@ -568,18 +568,10 @@ install_vim() {
 	# install .vim files
 	sudo rm -rf "${HOME}/.vim"
 	git clone --recursive git@github.com:jessfraz/.vim.git "${HOME}/.vim"
-	ln -snf "${HOME}/.vim/vimrc" "${HOME}/.vimrc"
-	sudo ln -snf "${HOME}/.vim" /root/.vim
-	sudo ln -snf "${HOME}/.vimrc" /root/.vimrc
-
-	# alias vim dotfiles to neovim
-	mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
-	ln -snf "${HOME}/.vim" "${XDG_CONFIG_HOME}/nvim"
-	ln -snf "${HOME}/.vimrc" "${XDG_CONFIG_HOME}/nvim/init.vim"
-	# do the same for root
-	sudo mkdir -p /root/.config
-	sudo ln -snf "${HOME}/.vim" /root/.config/nvim
-	sudo ln -snf "${HOME}/.vimrc" /root/.config/nvim/init.vim
+	(
+	cd "${HOME}/.vim"
+	make install
+	)
 
 	# update alternatives to neovim
 	sudo update-alternatives --install /usr/bin/vi vi "$(which nvim)" 60
