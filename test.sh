baf8619f9f81e65344072d6ee2e9f5549668c400
R.B. Boyer <public@richardboyer.net>
2018-12-01 21:11:54 -0600

    fix trivial typo (#31)

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ ERRORS=()
 for f in $(find . -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		{
-			shellcheck "$f" && echo "[OK]: sucessfully linted $f"
+			shellcheck "$f" && echo "[OK]: successfully linted $f"
 		} || {
 			# add to errors
 			ERRORS+=("$f")
