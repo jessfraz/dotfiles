39c4c25386115b9b2a2663a610a0a7b4aaec4cd6
Szymon Bialkowski <bialkowski.sz@gmail.com>
2019-01-02 15:38:49 +0000

    make find compatible with alpine (#32)

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -o pipefail
 ERRORS=()
 
 # find all executables and run `shellcheck`
-for f in $(find . -type f -not -iwholename '*.git*' -not -name "yubitouch.sh" | sort -u); do
+for f in $(find . -type f -not -path '*.git*' -not -name "yubitouch.sh" | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		{
 			shellcheck "$f" && echo "[OK]: successfully linted $f"
