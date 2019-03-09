9a7ec41d878e8416a52614002c66f1e02d4e4f47
Jess Frazelle <acidburn@jessfraz.com>
2019-03-09 15:37:26 -0500

    fix script
    Signed-off-by: Jess Frazelle <acidburn@jessfraz.com>

--- bin/install.sh
+++ bin/install.sh
@@ -475,7 +475,7 @@ install_wmapps() {
 	local pkgs=( feh i3 i3lock i3status scrot suckless-tools )
 
 	apt update || true
-	apt install -y "${pkgs[@]} " \
+	apt install -y ${pkgs[@]}  \
 		alsa-utils \
 		rxvt-unicode-256color \
 		usbmuxd \
